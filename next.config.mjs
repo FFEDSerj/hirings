0111caac5ddfdfab986cfdfe508aae1fdcf278d3
FFEDSerj <ffedchik@gmail.com>
2023-01-11 12:19:04 +0200

    updated images.domains

--- next.config.mjs
+++ next.config.mjs
@@ -14,11 +14,7 @@ const config = {
     defaultLocale: 'en',
   },
   images: {
-    domains: [
-      'avatars.githubusercontent.com',
-      'tailwindui.com',
-      'raw.githubusercontent.com',
-    ],
+    domains: ['avatars.githubusercontent.com', 'tailwindui.com'],
   },
 };
 export default config;

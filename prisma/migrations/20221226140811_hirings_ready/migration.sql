5e2f17fce1f2fe646af6b972b394b4ca687814fe
FFEDSerj <ffedchik@gmail.com>
2023-01-11 10:19:35 +0200

    removing migrations, that could cause issues with deploy

--- prisma/migrations/20221226140811_hirings_ready/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "companies" ADD COLUMN     "ceo" TEXT,
-ALTER COLUMN "budget" DROP NOT NULL,
-ALTER COLUMN "staff" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "hirings" ALTER COLUMN "description" DROP NOT NULL;
